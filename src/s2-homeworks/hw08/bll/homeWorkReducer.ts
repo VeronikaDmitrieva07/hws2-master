0d89ec3342eeb3a1b844cd1f4e75a5e806fa5965
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2025-12-14 01:05:49 +0300

    hw 8 done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortState = [...state]
+            return action.payload === "down"
+                ? sortState.sort((a, b) => b.name.localeCompare(a.name))
+                : sortState.sort((a, b) => a.name.localeCompare(b.name))
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= 18);
         }
         default:
             return state
